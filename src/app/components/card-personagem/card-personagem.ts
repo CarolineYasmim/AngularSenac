db69cb66ab4706918f150931fcf067c2a2ccb875
CarolineYasmim <rm558901@fiap.com.br>
2025-08-22 20:54:21 -0300

    class 12 (necessário correção)

---
+++ src/app/components/card-personagem/card-personagem.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-card-personagem',
+  imports: [RouterLink],
+  templateUrl: './card-personagem.html',
+  styleUrl: './card-personagem.css',
+})
+export class CardPersonagem {
+  nome = input.required<string>();
+  imagem = input.required<string>();
+  totalVotos = input.required<number>();
+  id = input.required<number>();
+
+  votou = output<{id:number; totalVotos:number}>();
+
+}
